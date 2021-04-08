f1bc23b536ec5250dc880e94bd56d056df6002d3
4ebula <4ebula@gmail.com>
2021-04-08 11:48:49 +0300

    feat: solve 11th task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = [...str];
+  const answer = [];
+  let current = [0, ''];
+  arr.push('');
+  arr.forEach((e) => {
+    if (e !== current[1]) {
+      answer.push((current[0] ? current[0] + 1 : '') + current[1]);
+      current = [0, e];
+    } else current[0]++;
+  });
+  return answer.join('');
 }
 
 module.exports = encodeLine;
