a860865b2d7a7776d9fd65814005625680de2c1b
4ebula <4ebula@gmail.com>
2021-04-08 10:40:37 +0300

    feat: solve 10th task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,16 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  function searchBin(start, end, val) {
+    let pivot = Math.floor((start + end) / 2);
+    if (val !== array[pivot]) {
+      if (val > array[pivot]) pivot = searchBin(pivot, end, val);
+      else pivot = searchBin(start, pivot, val);
+    }
+    return pivot;
+  }
+  return searchBin(0, array.length, value);
 }
 
 module.exports = findIndex;
