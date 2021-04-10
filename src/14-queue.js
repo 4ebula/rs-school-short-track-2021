4f5052ab0c0c6e89027c360d914c69b60b102445
4ebula <4ebula@gmail.com>
2021-04-10 16:00:29 +0300

    fix: make enqueue method work

--- src/14-queue.js
+++ src/14-queue.js
@@ -12,30 +12,30 @@ const ListNode = require('../extensions/list-node');
  */
 
 class Queue {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-    console.log('THIS', this, '\n\n');
-  }
-
   get size() {
     let depth = 0;
-    for (let i = this; i.next !== null; i = this.next, depth++);
+    if (['next'] in this) {
+      depth = 1;
+      for (let i = this; i.next !== null; i = i.next, depth++);
+    }
     return depth;
   }
 
   enqueue(element) {
-    const depth = this.size;
+    console.log('Enqueue: ', element);
     let endNode = this;
-    for (let i = 0; i < depth; endNode = this.next, i++);
-    console.log('THIS endnode', endNode, 'End of this');
-    endNode.next = new ListNode(element);
-    console.log(this);
+    if (this.size === 0) {
+      Object.assign(endNode, new ListNode(element));
+    } else {
+      for (; endNode.next !== null; endNode = endNode.next);
+      endNode.next = new ListNode(element);
+    }
+    console.log('THIS: ', this);
+    console.log('\n\n');
   }
 
   dequeue() {
-    return this.next;
-    // throw new Error('Not implemented');
+    throw new Error('Not implemented');
   }
 }
 
